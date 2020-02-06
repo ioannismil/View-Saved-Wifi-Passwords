76c863882b6bfa8092ef30f57d1c0006ad71b3e8
IoannisM <miltiadouy@gmail.com>
2020-02-06 20:40:03 +0200

    Added the ability to view saved wifi networks

--- getwifipassword.bat
+++ getwifipassword.bat
@@ -1,4 +1,6 @@
 @echo off
+ECHO Available Wifis:
+netsh wlan show profile
 set /P wifi="Enter wifi name: "
 ECHO The password is: 
 netsh wlan show profile "%wifi%" key=clear | findstr "Key Content"
