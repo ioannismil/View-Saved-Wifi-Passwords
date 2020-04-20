6eb5c69053647d74638eb9eac595a2d37ca4f83d
IoannisM <miltiadouy@gmail.com>
2020-04-20 21:49:22 +0300

    Update getwifipassword.bat
    added loop

--- getwifipassword.bat
+++ getwifipassword.bat
@@ -1,7 +1,9 @@
 @echo off
 ECHO Available Wifis:
 netsh wlan show profile
+:loophere
 set /P wifi="Enter wifi name: "
 ECHO The password is: 
 netsh wlan show profile "%wifi%" key=clear | findstr "Key Content"
+goto:loophere
 PAUSE
